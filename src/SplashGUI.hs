727c8e2b5be24a6621c2257ae3a28ab19b77d921
Oskar Mendel <brokenprogrammer@gmail.com>
2018-02-26 10:19:06 +0100

    Added responses for NewDialog and manual initialization of combobox.

--- src/SplashGUI.hs
+++ src/SplashGUI.hs
@@ -3,7 +3,7 @@
 module SplashGUI where
 
 import Graphics.UI.Gtk hiding (Action, backspace)
-import Graphics.UI.Gtk.Windows.Dialog
+import Data.Text as Text
 
 data SplashGUI = SplashGUI {
     splashWnd    :: Window,
@@ -16,6 +16,7 @@ data SplashGUI = SplashGUI {
 data NewDialogGUI = NewDialogGUI {
     dialogWnd       :: Dialog,
     dialogNme       :: Entry,
+    dialogTemplate  :: ComboBox,
     dialogOk        :: Button,
     dialogCnl       :: Button
 }
@@ -54,13 +55,22 @@ loadNewDialogGlade gladePath = do
     dialog <- builderGetObject builder castToDialog "NewDialog"
 
     entr <- builderGetObject builder castToEntry "NewDialogName"
+    combo <- builderGetObject builder castToComboBox "NewDialogTemplate"
     btnOk <- builderGetObject builder castToButton "NewDialogOk"
     btnCncl <- builderGetObject builder castToButton "NewDialogCancel"
 
-    return $ NewDialogGUI dialog entr btnOk btnCncl
+    return $ NewDialogGUI dialog entr combo btnOk btnCncl
 
 connectDialogGUI :: NewDialogGUI -> IO (ConnectId Button)
 connectDialogGUI dialog = do
+
+    -- TODO: Add this to data file or other structure. Oskar Mendel 2018-02-26
+    -- Has to be initializes through haskell.
+    -- Source: https://stackoverflow.com/questions/11990554/combobox-in-gtk2hs-glade
+    comboBoxSetModelText (dialogTemplate dialog)
+    comboBoxAppendText (dialogTemplate dialog) $ pack "Project Plan"
+    comboBoxAppendText (dialogTemplate dialog) $ pack "Test Plan"
+
     on (dialogCnl dialog) buttonActivated (widgetHide (dialogWnd dialog))
 
 splashNewAction :: IO ()
@@ -72,7 +82,18 @@ splashNewAction = do
     -- before returning control flow to your code, you can call dialogRun.
     connectDialogGUI dialog
 
-    widgetShowAll (dialogWnd dialog)
+    result <- dialogRun (dialogWnd dialog)
+    name <- (entryGetText (dialogNme dialog))
+    template <- (comboBoxGetActiveText (dialogTemplate dialog))
+
+    -- TODO: Any way to use Stock responses of ResponseOk etc.. ?
+    -- Oskar Mendel 2018-02-26
+    case result of 
+        ResponseUser 1      -> putStrLn ("ResponseUser " ++ name ++ " " ++ show template ++ show 1)
+        ResponseUser 0      -> putStrLn ("ResponseUser "++ show 0)
+        _                   -> error "Invalid Response"
+
+    widgetDestroy (dialogWnd dialog)
 
 main :: FilePath -> IO ()
 main gladePath = do
