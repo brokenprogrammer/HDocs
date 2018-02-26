16beca9496705a21918f9afda19a06c502492c16
Oskar Mendel <brokenprogrammer@gmail.com>
2018-02-26 05:38:46 +0100

    Moved SplashScreen specific code to SplashGUI.hs

--- src/Main.hs
+++ src/Main.hs
@@ -1,41 +1,47 @@
 module Main where
 
-import Control.Monad
-import Control.Monad.IO.Class
-import Data.IORef
-import Graphics.UI.Gtk hiding (Action, backspace)
-import Graphics.UI.Gtk.Glade
-import Graphics.UI.Gtk.Builder
+-- import Control.Monad
+-- import Control.Monad.IO.Class
+-- import Data.IORef
+-- import Graphics.UI.Gtk hiding (Action, backspace)
+-- import Graphics.UI.Gtk.Glade
+-- import Graphics.UI.Gtk.Builder
 
-test:: IO ()
-test = putStrLn "EDIT"
+import qualified SplashGUI
 
--- TODO: 2. Quit knapp 
--- TOOD: 3. New knapp - Öppnar main fönstret ?? 
+-- TOOD: 1. New knapp
+    -- Steps: 
+        -- 1. Användare klickar på New
+        -- 2. Message dialog, frågar efter Namn och Template (Template från dropdown)
+        -- 3. Användare väljer och klickar okej
+        -- 4. Visa main fönstret med laddad tempalte.
 
 main :: IO ()
 main = do
-    initGUI
+    SplashGUI.main "./data/Splash.glade"
+    -- initGUI
 
-    -- Load glade file
-    builder <- builderNew
-    builderAddFromFile builder "./data/Splash.glade"
+    -- -- Load glade file
+    -- builder <- builderNew
+    -- builderAddFromFile builder "./data/Splash.glade"
 
-    -- Get a handle to some of the widgets of the glade file.
-    window <- builderGetObject builder castToWindow "SplashWnd"
-    btnNew <- builderGetObject builder castToButton "SplashNew"
-    btnEdt <- builderGetObject builder castToButton "SplashEdit"
+    -- -- Get a handle to some of the widgets of the glade file.
+    -- window <- builderGetObject builder castToWindow "SplashWnd"
+    -- btnNew <- builderGetObject builder castToButton "SplashNew"
+    -- btnEdt <- builderGetObject builder castToButton "SplashEdit"
+    -- btnQut <- builderGetObject builder castToButton "SplashQuit"
 
-    -- set window [ windowTitle := "HDocs",
-    --              windowResizable := False,
-    --              windowDefaultWidth := 800,
-    --              windowDefaultHeight := 600 ]
+    -- -- set window [ windowTitle := "HDocs",
+    -- --              windowResizable := False,
+    -- --              windowDefaultWidth := 800,
+    -- --              windowDefaultHeight := 600 ]
 
-    -- Do something with the widgets to test..
-    on btnNew buttonActivated (putStrLn "NEW ITEM.")
-    on btnEdt buttonActivated test
-    -- When the window is destroeyd quit the application.
-    on window objectDestroy mainQuit
+    -- -- Do something with the widgets to test..
+    -- on btnNew buttonActivated (putStrLn "NEW ITEM.")
+    -- on btnEdt buttonActivated test
+    -- on btnQut buttonActivated mainQuit
+    -- -- When the window is destroeyd quit the application.
+    -- on window objectDestroy mainQuit
 
-    widgetShowAll window
-    mainGUI
+    -- widgetShowAll window
+    -- mainGUI
---
+++ src/SplashGUI.hs
@@ -0,0 +1,48 @@
+module SplashGUI where
+
+import Graphics.UI.Gtk hiding (Action, backspace)
+import Graphics.UI.Gtk.Builder
+
+data GUI = GUI {
+    splashWnd    :: Window,
+    splashNew    :: Button,
+    splashEdt    :: Button,
+    splashSet    :: Button,
+    splashQut    :: Button
+}
+
+loadSplashGlade :: FilePath -> IO GUI
+loadSplashGlade gladePath = do
+    builder <- builderNew
+    builderAddFromFile builder gladePath
+
+    -- Load Splash Window
+    window <- builderGetObject builder castToWindow "SplashWnd"
+
+    [btnNew, btnEdt, btnSet, btnQut] <-
+        mapM (builderGetObject builder castToButton)
+        ["SplashNew", "SplashEdit", "SplashSettings", "SplashQuit"]
+
+    return $ GUI window btnNew btnEdt btnSet btnQut
+
+connectGUI :: GUI -> IO (ConnectId Button)
+connectGUI gui = do
+    on (splashWnd gui) objectDestroy mainQuit
+
+    on (splashNew gui) buttonActivated (putStrLn "New")
+    on (splashEdt gui) buttonActivated (putStrLn "Edit")
+    on (splashSet gui) buttonActivated (putStrLn "Settings")
+    on (splashQut gui) buttonActivated mainQuit
+
+main :: FilePath -> IO ()
+main gladePath = do
+    initGUI
+
+    -- Initialize Splash screen GUI from the glade path.
+    gui <- loadSplashGlade gladePath
+
+    -- Connect the GUI to actionlisteners
+    connectGUI gui
+
+    widgetShowAll (splashWnd gui)
+    mainGUI
