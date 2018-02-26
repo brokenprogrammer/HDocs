1d129b3046ee76548fb4bf04eb76607efcba71fc
Oskar Mendel <brokenprogrammer@gmail.com>
2018-02-26 05:01:47 +0100

    Added displaying of the file list and removed old code.

--- src/Main.hs
+++ src/Main.hs
@@ -7,40 +7,35 @@ import Graphics.UI.Gtk hiding (Action, backspace)
 import Graphics.UI.Gtk.Glade
 import Graphics.UI.Gtk.Builder
 
+test:: IO ()
+test = putStrLn "EDIT"
+
+-- TODO: 2. Quit knapp 
+-- TOOD: 3. New knapp - Öppnar main fönstret ?? 
+
 main :: IO ()
 main = do
     initGUI
 
     -- Load glade file
-    -- splashXMLM <- xmlNew "Splash.glade"
-    -- let splashXML = case splashXMLM of
-    --         (Just splashXML) -> splashXML
-    --         Nothing          -> error "Cannong find the glade file \"Splash.glade\"."
-
     builder <- builderNew
     builderAddFromFile builder "./data/Splash.glade"
 
     -- Get a handle to some of the widgets of the glade file.
-    -- window <- xmlGetWidget splashXML castToWindow "SplashWnd"
-    -- btnNew <- xmlGetWidget splashXML castToButton "SplashNew"
     window <- builderGetObject builder castToWindow "SplashWnd"
     btnNew <- builderGetObject builder castToButton "SplashNew"
-
-
-    -- Do something with the widgets to test..
-    --btnNew `onClicked` putStrLn "New Item.."
-   -- window `onDestroy` mainQuit
-    on btnNew buttonActivated (putStrLn "NEW ITEM.")
-    on window objectDestroy mainQuit
-    -- window <- windowNew
+    btnEdt <- builderGetObject builder castToButton "SplashEdit"
 
     -- set window [ windowTitle := "HDocs",
     --              windowResizable := False,
     --              windowDefaultWidth := 800,
     --              windowDefaultHeight := 600 ]
 
-    -- -- When the window is destroyed quit the application.
-    -- on window objectDestroy mainQuit
+    -- Do something with the widgets to test..
+    on btnNew buttonActivated (putStrLn "NEW ITEM.")
+    on btnEdt buttonActivated test
+    -- When the window is destroeyd quit the application.
+    on window objectDestroy mainQuit
 
     widgetShowAll window
     mainGUI
